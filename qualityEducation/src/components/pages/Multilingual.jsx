079fd842ba0e9911bdf183d22d8785b72d360667
Jasleen Kaur <jka349@sfu.ca>
2025-03-12 04:19:41 -0700

    added navbar and pages

---
+++ qualityEducation/src/components/pages/Multilingual.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Multilingual() {
+  return (
+    <>
+      <h1 className='multilingual'>MULTILINGUAL</h1>
+    </>
+  );
+}
---
+++ qualityEducation/src/components/pages/Skills.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Skills() {
+  return (
+    <>
+      <h1 className='skills'>SKILLS</h1>
+    </>
+  );
+}
