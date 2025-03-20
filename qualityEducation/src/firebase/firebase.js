b4f014c6e5e929ba086fa9a6708fe9fe01abddfe
noodels <daniel.li120406@gmail.com>
2025-03-19 19:55:18 -0700

    add firebase to project

---
+++ qualityEducation/src/firebase/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { dotenv } from "dotenv";
+dotenv.config();
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "learnverse-hack-the-sem-2025.firebaseapp.com",
+  projectId: "learnverse-hack-the-sem-2025",
+  storageBucket: "learnverse-hack-the-sem-2025.firebasestorage.app",
+  messagingSenderId: "737545318031",
+  appId: "1:737545318031:web:0518fbef05eb5bf0924afa",
+  measurementId: "G-9WJLPCK6N6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider };
