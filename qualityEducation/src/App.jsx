0df8e6da0589052c478532388be2950eece3ce11
Jasleen Kaur <jka349@sfu.ca>
2025-03-28 01:45:30 -0700

    removed community and started contacts page

--- qualityEducation/src/App.jsx
+++ qualityEducation/src/App.jsx
@@ -6,7 +6,6 @@ import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Contact from './pages/Contact';
-import Community from './pages/Community';
 import Multilingual from './pages/Multilingual';
 import Skills from './pages/Skills';
 import Login from './pages/Login';
@@ -22,7 +21,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/community" element={<Community />} />
         <Route path="/multilingual" element={<Multilingual />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/login" element={<Login />} />
